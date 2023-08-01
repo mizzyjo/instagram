9d957f8caf13cc74c9bbe19a60548ce31818c2f7
mizzyjo <jomizziie@gmail.com>
2023-08-01 20:15:11 +0900

    add more posts from server

---
+++ src/components/AppContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  THIS IS CONTAINER
+  <post-item
+    :postData="post"
+    v-for="(post, idx) in postData"
+    :key="idx"
+  ></post-item>
+</template>
+
+<script>
+import PostItem from './PostItem.vue';
+export default {
+  components: {
+    PostItem,
+  },
+  props: {
+    postData: Array,
+  },
+};
+</script>
+
+<style></style>
